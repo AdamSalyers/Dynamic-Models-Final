70c6d3e31dcac29ba652b37cd59475ec250fa272
LonelyRider-cs <54919991+LonelyRider-cs@users.noreply.github.com>
2020-05-04 13:01:17 -0600

    fixed plotting issue

--- Final-Project.py
+++ Final-Project.py
@@ -6,7 +6,7 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-import time
+
 
 class flock():
     def init_wrm(self, ln_wrm, p):
@@ -15,6 +15,7 @@ class flock():
         my_wrms = []#a matrix of worm locations
         for pos in p:
             #print(pos)
+            
             pos = pos.tolist()
             #print("Pos is:", pos)
             visited = [pos] #keep track of position already in the worm and to will be the list of the final worm.
@@ -43,6 +44,7 @@ class flock():
                             ln += 1
                             visited.append([visited[-1][0], visited[-1][1]-1])
                             break
+            
             my_wrms.append(visited)#append our worm to the list of worms
         return my_wrms
                 
@@ -56,6 +58,7 @@ class flock():
 
             #Get cordinates of head of worm
             curr_worm_head = curr_worm[0]
+            #print(curr_worm_head)
             
             #print("velocity change for x:", v[i][0])
             new_head[0] = curr_worm_head[0] + v[i][0]#add v then make sure we stay in bounds
@@ -77,32 +80,32 @@ class flock():
         
         return wrm_pos
     
-        def run_sim(self, c1=0.001,c2=0.01,c3=1,c4=0.01):
-            N = 400 #No. of Worms
-            frames = 10 #No. of frames
-            limit = 100 #Axis Limits
-            L  = limit*2
-            ln_wrm = 5 #length of each worm
-            mu = 0.0 #starting point of our cluster
-            kappa = 10.0 #starting dispersion
-            pfinal = []
-            P = 10 #Spread of initial position (gaussian)
-            V = 10 #Spread of initial velocity (gaussian)
-            delta = 1 #Time Step
-            p = P*np.random.vonmises(mu,kappa, size=(N,2))#A random circular probabibility distribution
-            wrm_pos = self.init_wrm(ln_wrm,p)#Get our initial Worm Positions
-            print(len(wrm_pos))
-            print(len(wrm_pos[0]))
-            print(len(wrm_pos[0][0]))
-            #Initializing plot
-            plt.ion()
+    #def run_sim(self, c1=0.001,c2=0.01,c3=1,c4=0.01):
+    #        N = 400 #No. of Worms
+    #        frames = 10 #No. of frames
+    #        limit = 100 #Axis Limits
+    #        L  = limit*2
+    #        ln_wrm = 5 #length of each worm
+    #        mu = 0.0 #starting point of our cluster
+    #        kappa = 10.0 #starting dispersion
+    #        pfinal = []
+    #        P = 10 #Spread of initial position (gaussian)
+    #        V = 10 #Spread of initial velocity (gaussian)
+    #        delta = 1 #Time Step
+    #        p = P*np.random.vonmises(mu,kappa, size=(N,2))#A random circular probabibility distribution
+    #        wrm_pos = self.init_wrm(ln_wrm,p)#Get our initial Worm Positions
+    #        print(len(wrm_pos))
+    #        print(len(wrm_pos[0]))
+    #        print(len(wrm_pos[0][0]))
+    #        #Initializing plot
+    #        plt.ion()
             
             
-            for i in range(0, frames):
+            #for i in range(0, frames):
                 
     
-    def flocking_python(self,c1=0.001,c2=0.01,c3=1,c4=0.01):
-        N = 400 #No. of Worms
+    def flocking_python(self,c1=0.001,c2=0.01,c3=1,c4=0.5):
+        N = 200 #No. of Worms
         frames = 10 #No. of frames
         limit = 100 #Axis Limits
         L  = limit*2
@@ -134,7 +137,7 @@ class flock():
             v1 = np.zeros((N,2))
             v2 = np.zeros((N,2))
             
-            #YOUR CODE HERE
+            
             #Calculate Average Velocity v3 
             trans = np.transpose(v)
             v3 = [sum(trans[0]),sum(trans[1])]*c3
@@ -149,7 +152,7 @@ class flock():
                     p = np.array(wrm_pos[m])
                     pm = p.mean(axis=0)
                     if m!=n:
-                        #YOUR CODE HERE
+                        
                         #Compute vector r from one agent to the next
                         #print("p[m] is",p[m])
                         r = pm-pn
@@ -163,8 +166,7 @@ class flock():
                             r[1] = r[1]-L
                         elif r[1] < -L/2:
                             r[1] = r[1]+L
-
-                        #YOUR CODE HERE
+                            
                         #Compute distance between agents rmag
                         rmag = math.sqrt(r[0]**2+r[1]**2)
                         #Compute attraction v1
@@ -172,14 +174,12 @@ class flock():
                         #Compute Repulsion [non-linear scaling] v2
                         v2[n] -= (c2*r)/(rmag**2)
                    
-                #YOUR CODE HERE
                 #Compute random velocity component v4
                 v4 = c4*np.random.randn(1,2)
                 #Update velocity
                 #print("Shapes: v1:",v1[n].shape, "v2:",v2[n].shape,"v3:",v3.shape,v)
                 v[n] = v1[n]+v2[n]+v3+v4
                 
-            #YOUR CODE HERE
             #Update position
             v *= delta
             wrm_pos = self.upd_wrm(wrm_pos, v, L)#update worm position
@@ -188,7 +188,9 @@ class flock():
             plt.xlim(-limit, limit)
             plt.ylim(-limit, limit)
             for worms in wrm_pos:
-                plt.plot(worms)
+                x = [worms[0][0],worms[1][0],worms[2][0],worms[3][0],worms[4][0]]
+                y = [worms[0][1],worms[1][1],worms[2][1],worms[3][1],worms[4][1]]
+                plt.plot(x, y)
             plt.show()
             
             pfinal.append(wrm_pos)
@@ -200,5 +202,3 @@ flock_py = flock()
 c1dist = []
 
 pfinal = flock_py.flocking_python()
-    
-
